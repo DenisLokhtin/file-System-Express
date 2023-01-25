d9590e8aad54cfd75bcebc5b349f56e0a75ba89e
DenisLokhtin <denis.lokhtin@gmail.com>
2023-01-25 11:00:29 +0600

    add file update

--- app/file.js
+++ app/file.js
@@ -1,20 +1,26 @@
-const fs = require('fs');
 const path = require('path');
 const express = require("express");
 const WriteFile = require("../utils/WriteFile");
 const ReadFile = require("../utils/ReadFile");
-const {Client} = require('pg');
-const url = require("url");
 const pool = require("../db");
 const router = express.Router();
 const savePath = path.join(__dirname, '../uploads/');
 
 
 router.put('/:fileName', async (req, res) => {
-    await pool.query(
-        "INSERT INTO file (path, name, mimeType, size) VALUES ($1, $2, $3, $4)",
-        [savePath, req.params.fileName, req.headers['content-type'], req.headers['content-length']]
-    );
+    const file = await pool.query(`SELECT * FROM file WHERE name = '${req.params.fileName}' LIMIT 1`)
+
+    if (!file.rows.length) {
+        await pool.query(
+            "INSERT INTO file (path, name, mimeType, size) VALUES ($1, $2, $3, $4)",
+            [savePath, req.params.fileName, req.headers['content-type'], req.headers['content-length']]
+        );
+    } else {
+        await pool.query(
+            "UPDATE file SET path = $1, mimetype = $2, size =  $3 WHERE name = $4;",
+            [savePath, req.headers['content-type'], req.headers['content-length'], req.params.fileName]
+        )
+    }
     await WriteFile(req, res);
 });
 
