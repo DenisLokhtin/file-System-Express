1495185d7e098aad5ad09a0845757c14dc002dba
DenisLokhtin <denis.lokhtin@gmail.com>
2023-01-27 13:58:06 +0600

    minor changes

--- configs/storage-config.js
+++ configs/storage-config.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const postgres = {
     host: process.env.POSTGRES_HOST,
-    port: Number(process.env.POSTGRES_PORT),
+    port: +process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DATABASE,
